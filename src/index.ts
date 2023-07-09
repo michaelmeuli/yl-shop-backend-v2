a38b06be998c742f712a452cc0185abfe549beff
Michael Meuli <michael.meuli@gmail.com>
2023-07-09 15:19:09 +0200

    test run-populate.ts

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-import { bootstrap, runMigrations } from '@vendure/core';
-import { config } from './vendure-config';
-import { populateOnFirstRun } from './populate';
-
-populateOnFirstRun(config)
-    .then(() => runMigrations(config))
-    .then(() => bootstrap(config))
-    .catch(err => {
-        console.log(err);
-    });
-
-
 // import { bootstrap, runMigrations } from '@vendure/core';
 // import { config } from './vendure-config';
+// import { populateOnFirstRun } from './populate';
 
-// runMigrations(config)
+// populateOnFirstRun(config)
+//     .then(() => runMigrations(config))
 //     .then(() => bootstrap(config))
 //     .catch(err => {
 //         console.log(err);
 //     });
+
+
+import { bootstrap, runMigrations } from '@vendure/core';
+import { config } from './vendure-config';
+
+runMigrations(config)
+    .then(() => bootstrap(config))
+    .catch(err => {
+        console.log(err);
+    });
